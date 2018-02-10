36071778c6c5bcb8df7948acf70e2c5874a06cfa
eidercarlos <eidercarlos@gmail.com>
2018-02-09 23:33:04 -0200

    Added an sphere sample and a simple script for rotation aroud the x and y axis

---
+++ Assets/Scripts/SphereInteraction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereInteraction : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0f;
+    public float verticalSpeed = 2.0f;
+
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(v, -h, 0, Space.World);
+    }
+}
